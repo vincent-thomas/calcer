df41efb9f25e265dbd8e1a615378d88fda41db5b
Vincent Thomas <77443389+vincent-thomas@users.noreply.github.com>
2024-05-20 00:10:08 +0200

    backup

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,31 @@
 use calcer::problem::Problem;
 use clier::{
-    display::Displayer,
+    display::{label::LabelLogger, Displayer},
     hooks::{use_double_dash, use_flag, FlagError},
     run::ExitCode,
     CliMeta, Clier, CmdMeta, Commands,
 };
 
+const VERSION: &str = env!("CARGO_PKG_VERSION");
+
 fn meta() -> CliMeta {
-    CliMeta {
-        name: "clier".to_string(),
-        description: "Clier is a nice math thing".to_string(),
-        version: Some((0, 1, 0)),
-        usage: Some("[command]".to_string()),
-    }
+    CliMeta::new(
+        "clier".to_string(),
+        "Clier is a nice math thing".to_string(),
+    )
+    .version(VERSION)
+    .usage("[command]")
 }
 
 fn solve_command() -> Commands {
     Commands::Command {
         meta: CmdMeta::new("solve", "only do problems"),
         handler: |clier| {
-            let error = Displayer::Error {};
+            let log = LabelLogger::default();
             let problem_str = match use_double_dash(&clier) {
                 Ok(value) => value,
                 Err(_) => {
-                    error.write("Problem is required and should be defined after '--'");
+                    log.error("Problem is required and should be defined after '--'");
                     return ExitCode(1);
                 }
             };
@@ -38,7 +40,7 @@ fn graph_command() -> Commands {
     Commands::Command {
         meta: CmdMeta::new("graph", "Use for graphing math problem with 'x'"),
         handler: |clier| {
-            let error = Displayer::Error {};
+            let log = LabelLogger::default();
 
             let flag_result: Result<String, FlagError> =
                 use_flag("definition", Some('d'), &clier).try_into();
@@ -46,7 +48,7 @@ fn graph_command() -> Commands {
             let flag = match flag_result {
                 Ok(value) => value,
                 Err(_) => {
-                    error.write("Missing range");
+                    log.error("Missing range");
                     return ExitCode(1);
                 }
             };
@@ -55,14 +57,14 @@ fn graph_command() -> Commands {
             let start: i64 = match raw_tal[0].parse() {
                 Ok(value) => value,
                 Err(_) => {
-                    error.write("Failed to parse start range");
+                    log.error("Failed to parse start range");
                     return ExitCode(1);
                 }
             };
             let end: i64 = match raw_tal[1].parse() {
                 Ok(value) => value,
                 Err(_) => {
-                    error.write("Failed to parse end range");
+                    log.error("Failed to parse end range");
                     return ExitCode(1);
                 }
             };
@@ -70,7 +72,7 @@ fn graph_command() -> Commands {
             let problem_str = match use_double_dash(&clier) {
                 Ok(value) => value,
                 Err(_) => {
-                    error.write("Problem is not defined");
+                    log.error("Problem is not defined");
                     return ExitCode(1);
                 }
             };
